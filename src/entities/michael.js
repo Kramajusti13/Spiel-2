8c0eaaf25c3d2f67781e7d9d78e37ba19e383e2d
Kramajusti13 <schwenkjustus@gmail.com>
2026-09-09 00:48:45 +0200

    Fix michael.js: swordReturnSpeed undefined error + line breaks in string/code
    - Add def: this.def to sword object so this.def.swordReturnSpeed resolves
      (sword.update() referenced this.def but the object had no def property,
      causing 'Cannot read properties of undefined (reading swordReturnSpeed)')
    - Fix line break inside 'teleportToSword' string literal
    - Fix line break between '=' and 'dist' in sword update method

--- src/entities/michael.js
+++ src/entities/michael.js
@@ -171,6 +171,7 @@ export class Michael extends Enemy {
         ctx.fill();
         ctx.restore();
       },
+      def: this.def,
       owner: this
     };
     
@@ -201,7 +202,8 @@ export class Michael extends Enemy {
         }
         
         const player = game.player;
-        const d = dist(this.x, this.y, player.x, player.y);
+        const d = dist(this.x, this
+.y, player.x, player.y);
         if (d <= this.radius + player.hw && !this.hit) {
           player.takeDamage(this.damage, 0, game);
           this.hit = true;
